a7f5690a3e1a7afac79316682eb2ff3dde7a4944
tomaszkrysiuk <tomasz.krysiuk@gmail.com>
2015-10-08 11:49:14 +0200

    main commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <funkcje.h>
+#include <mapa_funkcji.h>
+using namespace std;
 
+int main()
+{
+    cout<<"witaj!"<<endl<<endl;
+    DostepneFunkcje(mapa_funkcji);
+
+    cout<< "\npodaj nazwe funkcji: ";
+
+    string WybranaFunkcja;
+    getline(cin,WybranaFunkcja);
+
+    wykonaj(mapa_funkcji,WybranaFunkcja);
+
+
+    return 0;
+}
